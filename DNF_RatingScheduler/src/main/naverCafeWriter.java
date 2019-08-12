b818da96d51117d05e17b1230b2ee39f10a93a43
TaehyunGong <38848719+TaehyunGong@users.noreply.github.com>
2019-08-12 23:16:49 +0900

    20190812 - v0.1 개발 완료.

--- DNF_RatingScheduler/src/main/naverCafeWriter.java
+++ DNF_RatingScheduler/src/main/naverCafeWriter.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -37,41 +38,47 @@ public class naverCafeWriter {
 	propertyGetAPIKEY getkey = propertyGetAPIKEY.getInstance();
 	
 	public void cafeWrtier() throws IOException {
-		getkey.initProperty("resources/APIKEY.properties");		
-
+		//apikey를 가져옴
+		getkey.initProperty("resources/APIKEY.properties");
+		
 		String apiURL = "https://openapi.naver.com/v1/cafe/29837103/menu/1/articles";
-		Map<String, String> map = new HashMap<String, String>();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 등급 ");
 		
 		DnfItemRating dnf = new DnfItemRating();
-		String subject = new Date().toString();	//글 제목
-		String content = contentHtmlMake(dnf);	//글 본문
+		List<Equipment> list = dnf.ratingItem(dnf.getEquipment(), getkey.getKeyBox().get(getkey.DNF_APIKEY));
 		
+		String subject = sdf.format(new Date()) + list.get(0).getItemGradeName() + "(" + requestItemAverageRating(list) + "%)";	//글 제목
+		String content = contentHtmlMake(dnf, list);	//글 본문
+		
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("subject", subject);
 		map.put("content", content);
+		
 		conn.HttpPostConnection(apiURL, RequestAccessToken(), map);
 	}
 	
 	// 카페 본문 내용
-	public String contentHtmlMake(DnfItemRating dnf) throws IOException {
+	public String contentHtmlMake(DnfItemRating dnf, List<Equipment> list) throws IOException {
 		StringBuffer sb = new StringBuffer();
 		
+		//sb.append("<h1>CTRL+F 로 원하는 장비 등급을 확인하세요.</h1>");
 		sb.append("<table style='width:100%;'>");
-		List<Equipment> list = dnf.ratingItem(dnf.getEquipment(), getkey.getKeyBox().get(getkey.DNF_APIKEY));
 		
 		String checkSetName = "";	// 첫번째 세트명
 		
 		for(Equipment equip : list){
 			if(!checkSetName.equals(nvlString(equip.getSetItemName(), "천공의 유산"))) {
-				sb.append("<tr><td colspan='4' style='font-size: 20px; font-weight: bold;'>" + nvlString(equip.getSetItemName(), "천공의 유산") + "</td> </tr>");
+				sb.append("<tr><td colspan='4' style='font-size: 20px; font-weight: bold;'> -" + nvlString(equip.getSetItemName(), "천공의 유산") + "</td> </tr>");
 			}
 			checkSetName = nvlString(equip.getSetItemName(), "천공의 유산");	// 세트명 
 			
 			sb.append("<tr>");
 			
-			sb.append("<td width='10%'> <img src='https://img-api.neople.co.kr/df/items/" + equip.getItemId() +"'> </td>");
+			sb.append("<td width='8%'> <img src='https://img-api.neople.co.kr/df/items/" + equip.getItemId() +"'> </td>");
 			sb.append("<td width='25%'> " + equip.getItemName() +" </td>");
-			sb.append("<td width='15%'> " + equip.getItemGradeName() + " : (" + equip.getItemGradeValue() + "%) </td>");
-			sb.append("<td width='50%'> " + htmlTagInsertList(equip.getMaxItemStatus(), equip.getItemStatus()) +" </td>");
+			sb.append("<td width='20%'> " + equip.getItemGradeName() + " (" + equip.getItemGradeValue() + "%) </td>");
+			sb.append("<td width='48%'> " + htmlTagInsertList(equip.getMaxItemStatus(), equip.getItemStatus()) +" </td>");
 			
 			sb.append("</tr>");
 			
@@ -79,6 +86,16 @@ public class naverCafeWriter {
 		
 		sb.append("</table>");
 		return sb.toString();
+	}
+	
+	//아이템 평균 등급
+	public int requestItemAverageRating(List<Equipment> list){
+		int sum = 0;
+		
+		for(Equipment equip : list)
+			sum += Integer.parseInt(equip.getItemGradeValue());
+		
+		return sum / list.size();
 	}
 	
 	//수치값과 차이점

a7811efe1f295257872e6c4a848aafe2c743e439
tony <tony950620@naver.com>
2019-08-20 15:39:44 +0900

    20190820 - 장비 DB insert 추가

--- DNF_RatingScheduler/src/dao/DnfItemRatingDao.java
+++ DNF_RatingScheduler/src/dao/DnfItemRatingDao.java
@@ -22,6 +22,13 @@ public class DnfItemRatingDao {
 		return DnfItemRatingDao_Singieton.instance;
 	}
 	
+	/**
+	 * @param conn
+	 * @param list
+	 * @return 0 or 1
+	 * @throws SQLException
+	 * @description 장비 옵션 DB INSERT
+	 */
 	public int insertStatus(Connection conn, List<Equipment> list) throws SQLException {
 		
 		List<String> itemId = new ArrayList<String>();
@@ -46,8 +53,6 @@ public class DnfItemRatingDao {
 		try {
 			ps = conn.prepareStatement(sql.toString());
 			
-//			ps.setArray(1, conn.createArrayOf("VARCHAR", itemId.toArray()));
-			
 			for(int i=0; i<itemId.size(); i++) {
 				ps.setString(1+(i*3), itemId.get(i));
 				ps.setString(2+(i*3), name.get(i));
@@ -67,4 +72,51 @@ public class DnfItemRatingDao {
 		return result;
 	}
 	
+	
+	/**
+	 * @param conn
+	 * @param list
+	 * @return 0 or 1
+	 * @throws SQLException 
+	 * @description 장비를 DB INSERT
+	 */
+	public int insertEquipment(Connection conn, List<Equipment> list) throws SQLException {
+		int result = 0;
+		PreparedStatement ps = null;
+		StringBuffer sql = new StringBuffer("INSERT INTO Equipment VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DATE_FORMAT(NOW(),'%Y%m%d') )");
+		
+		for(int n=0; n<list.size()-1; n++)
+			sql.append(", (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DATE_FORMAT(NOW(),'%Y%m%d') )");
+		
+		try {
+			ps = conn.prepareStatement(sql.toString());
+			
+			for(int i=0; i<list.size(); i++) {
+				Equipment equip = list.get(i);
+				
+				ps.setString(1+(i*13), equip.getItemId());
+				ps.setString(2+(i*13), equip.getItemName());
+				ps.setString(3+(i*13), equip.getItemRarity());
+				ps.setString(4+(i*13), equip.getItemType());
+				ps.setString(5+(i*13), equip.getItemTypeDetail());
+				ps.setInt(6+(i*13), equip.getItemAvailableLevel());
+				ps.setString(7+(i*13), equip.getItemObtainInfo());
+				ps.setString(8+(i*13), equip.getItemExplain());
+				ps.setString(9+(i*13), equip.getItemExplainDetail());
+				ps.setString(10+(i*13), equip.getItemFlavorText());
+				ps.setString(11+(i*13), equip.getSetItemId());
+				ps.setString(12+(i*13), equip.getSetItemName());
+				ps.setInt(13+(i*13), i+1);
+			}
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			ps.close();
+			conn.close();
+		}
+		
+		return result;
+	}
 }
